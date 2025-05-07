2bfe14a3df2fb6b48e48d6ac84d6f7e5fb577ccb
Vincent Nuis <vincent.nuis@gmail.com>
2025-05-07 14:42:50 +0200

    omg connectie

---
+++ src/main/java/spring/boot/Controller/HomeController.java
@@ -0,0 +1,13 @@
+package spring.boot.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Spring Boot is live on Railway!";
+    }
+}
